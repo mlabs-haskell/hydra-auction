a283b987044d5c05494c7ae8345c25e4eef58bd2
Giovanni Garufi <nazrhom@gmail.com>
2023-01-09 15:31:24 +0100

    Add initial hydra test

--- flake.nix
+++ flake.nix
@@ -15,16 +15,21 @@
 
   inputs = {
     # when you upgrade `hydra` input remember to also upgrade revs under `source-repository-package`s in `cabal.project`
-    hydra.url = "github:input-output-hk/hydra/fafb8e5eca0f5fc615f5957ac1243e09c19f9a9f";
-    # haskellNix.follows = "hydra/haskellNix";
-    haskellNix.url = "github:input-output-hk/haskell.nix";
+    hydra = {
+      url = "ssh://git@github.com/input-output-hk/hydra?ref=5a82f91b2b0e48328b1603d1140487e08f3c038f";
+      type = "git";
+      submodules = true;
+    };
+    cardano-node.url = "github:input-output-hk/cardano-node/a42ca8801ea31cb0b23a3f53dcc063ce4a5a0be5";
+    # We are using a forked version of haskellNix until this PR is merged: https://github.com/input-output-hk/haskell.nix/pull/1818
+    haskellNix.url = "github:input-output-hk/haskell.nix/ff43e6ce6ad2a630da9d02faa8cf97794496bda6";
     iohk-nix.follows = "hydra/iohk-nix";
     CHaP.follows = "hydra/CHaP";
     nixpkgs.follows = "hydra/nixpkgs";
     flake-utils.follows = "hydra/flake-utils";
   };
 
-  outputs = inputs@{ self, hydra, haskellNix, iohk-nix, CHaP, nixpkgs, flake-utils }:
+  outputs = inputs@{ self, hydra, haskellNix, iohk-nix, CHaP, nixpkgs, flake-utils, cardano-node }:
     let
       overlays = [
         haskellNix.overlay
@@ -35,35 +40,32 @@
               src = final.haskell-nix.haskellLib.cleanGit { src = ./.; name = "hydra-auction"; };
               inputMap = {
                 "https://input-output-hk.github.io/cardano-haskell-packages" = CHaP;
+                "https://github.com/input-output-hk/hydra" = hydra;
               };
-              # extraHackage = [
-              #   "${plutarch}"
-              #   "${plutarch}/plutarch-extra"
-              #   "${plutarch}/plutarch-test"
-              #   "${ply}/ply-core"
-              #   "${ply}/ply-plutarch"
-              # ];
               compiler-nix-name = "ghc8107";
               shell.tools = {
-                cabal = "3.4.0.0";
+                cabal = "3.6.2.0";
                 fourmolu = "0.4.0.0";
                 haskell-language-server = "latest";
               };
               shell.buildInputs = with final; [
                 nixpkgs-fmt
+                cardano-node.packages.${prev.system}.cardano-node
+                cardano-node.packages.${prev.system}.cardano-cli
+                hydra.packages.${prev.system}.hydra-node
               ];
-                modules = [
-                    # Set libsodium-vrf on cardano-crypto-{praos,class}. Otherwise they depend
-                    # on libsodium, which lacks the vrf functionality.
-                    ({ pkgs, lib, ... }:
-                        # Override libsodium with local 'pkgs' to make sure it's using
-                        # overriden 'pkgs', e.g. musl64 packages
-                        {
-                        packages.cardano-crypto-class.components.library.pkgconfig = lib.mkForce [ [ pkgs.libsodium-vrf pkgs.secp256k1 ] ];
-                        packages.cardano-crypto-praos.components.library.pkgconfig = lib.mkForce [ [ pkgs.libsodium-vrf ] ];
-                        }
-                    )
-                    ];
+              modules = [
+                # Set libsodium-vrf on cardano-crypto-{praos,class}. Otherwise they depend
+                # on libsodium, which lacks the vrf functionality.
+                ({ pkgs, lib, ... }:
+                  # Override libsodium with local 'pkgs' to make sure it's using
+                  # overriden 'pkgs', e.g. musl64 packages
+                  {
+                  packages.cardano-crypto-class.components.library.pkgconfig = lib.mkForce [ [ pkgs.libsodium-vrf pkgs.secp256k1 ] ];
+                  packages.cardano-crypto-praos.components.library.pkgconfig = lib.mkForce [ [ pkgs.libsodium-vrf ] ];
+                  }
+                )
+                ];
             };
         })
       ];
