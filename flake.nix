30556d0f93b3ec4c86ebc053a51cf1a2674a04d1
Giovanni Garufi <nazrhom@gmail.com>
2023-01-11 18:17:50 +0100

    Move exports around in flake

--- flake.nix
+++ flake.nix
@@ -78,7 +78,13 @@
       pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
       haskellNixFlake = pkgs.hydraProject.flake { };
       in {
-        packages.default = haskellNixFlake.packages."hydra-auction:exe:hydra-auction";
+        packages = {
+          default = haskellNixFlake.packages."hydra-auction:exe:hydra-auction";
+          check = pkgs.runCommand "combined-test" {
+            nativeBuildInputs = builtins.attrValues self.checks.${system};
+          } "touch $out";
+        };
+        inherit (haskellNixFlake) devShells;
         checks = builtins.mapAttrs (_: test: test.overrideAttrs (old: {
           nativeBuildInputs = old.nativeBuildInputs ++ [
             cardano-node.packages.${system}.cardano-node
@@ -86,16 +92,8 @@
             hydra.packages.${system}.hydra-node
           ];
         })) haskellNixFlake.checks;
-        check = pkgs.runCommand "combined-test" {
-          nativeBuildInputs = builtins.attrValues self.checks.${system};
-        } "touch $out";
         formatter.default = pkgs.nixpkgs-fmt;
       }) // {
-      herculesCI = {
-        ciSystems = [ "x86_64-linux" ];
-        outputs = {
-          packages = self.packages.x86_64-linux;
-        };
-      };
+      herculesCI.ciSystems = [ "x86_64-linux" ];
     };
 }
