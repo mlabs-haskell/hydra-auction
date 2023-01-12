caa0fd9b7336d985d18ae3ac1ecaa44f6d5c192f
Gregory Gerasev <uhbif19@gmail.com>
2023-01-13 00:57:13 +0400

    Fixed some review issues and linters

--- src/HydraAuction/Types.hs
+++ src/HydraAuction/Types.hs
@@ -3,9 +3,10 @@
 
 module HydraAuction.Types (AuctionTerms) where
 
-import Prelude
+import Prelude qualified
 
 import Data.Natural (Natural)
+import GHC.Generics (Generic)
 import Plutus.V1.Ledger.Contexts (TxOutRef)
 import Plutus.V1.Ledger.Crypto (PubKeyHash)
 import Plutus.V1.Ledger.Time (POSIXTime)
@@ -14,67 +15,77 @@ import Plutus.V1.Ledger.Value (AssetClass)
 -- Base datatypes
 
 data AuctionTerms = AuctionTerms
-  { auctionLot :: AssetClass
-  -- ^ What is being sold at the auction?
-  , seller     :: PubKeyHash
-  -- ^ Who is selling it?
-  , delegates :: [PubKeyHash]
-  -- ^ Who is running the Hydra Head where bidding occurs?
+  { -- | What is being sold at the auction?
+    auctionLot :: AssetClass
+  , -- | Who is selling it?
+    seller :: PubKeyHash
+  , -- | Who is running the Hydra Head where bidding occurs?
+    delegates :: [PubKeyHash]
   , biddingStart :: POSIXTime
   , biddingEnd :: POSIXTime
   , voucherExpiry :: POSIXTime
-  , cleanup :: POSIXTime
-  -- ^ Auction lifecycle times.
-  , auctionFee :: Natural
-  -- ^ Total auction fee that will be evenly split among delegates.
-  , startingBid :: Natural
-  -- ^ The auction lot cannot be sold for less than this bid price.
-  , minimumBidIncrement :: Natural
-  -- ^ A new bid can only supersede the standing bid if it is larger
-  -- by this increment.
-  , utxoRef :: TxOutRef
-  -- ^ The seller consumed this utxo input in the transaction that
-  -- announced this auction, to provide the auction lot to the auction.
+  , -- | Auction lifecycle times.
+    cleanup :: POSIXTime
+  , -- | Total auction fee that will be evenly split among delegates.
+    auctionFee :: Natural
+  , -- | The auction lot cannot be sold for less than this bid price.
+    startingBid :: Natural
+  , -- | A new bid can only supersede the standing bid if it is larger
+    -- by this increment.
+    minimumBidIncrement :: Natural
+  , -- | The seller consumed this utxo input in the transaction that
+    -- announced this auction, to provide the auction lot to the auction.
+    utxoRef :: TxOutRef
   }
+  deriving stock (Generic, Prelude.Show, Prelude.Eq)
 
-data ApprovedBidders = ApprovedBidders
-  { bidders :: [PubKeyHash]
-  -- ^ Which bidders are approved to submit bids?
+newtype ApprovedBidders = ApprovedBidders
+  { -- | Which bidders are approved to submit bids?
+    bidders :: [PubKeyHash]
   }
+  deriving stock (Generic, Prelude.Show, Prelude.Eq)
 
 data StandingBidState = NoBid | Bid BidTerms
+  deriving stock (Generic, Prelude.Show, Prelude.Eq)
 
 data BidTerms = BidTerms
-  { bidder :: PubKeyHash
-  -- ^ Who submitted the bid?
-  , bidPrice :: Natural
-  -- ^ Which price did the bidder set to buy the auction lot?
+  { -- | Who submitted the bid?
+    bidder :: PubKeyHash
+  , -- | Which price did the bidder set to buy the auction lot?
+    bidPrice :: Natural
   }
+  deriving stock (Generic, Prelude.Show, Prelude.Eq)
 
-data AuctionState =
-    Announced
+data AuctionState
+  = Announced
   | BiddingStarted ApprovedBiddersHash
   | Complete
+  deriving stock (Generic, Prelude.Show, Prelude.Eq)
 
 -- TODO: String will be changed to actuall hash
-data ApprovedBiddersHash = ApprovedBiddersHash String
--- ^ This hash is calculated from the `ApprovedBidders` value that the seller
--- fixes for the auction.
+newtype ApprovedBiddersHash = ApprovedBiddersHash Prelude.String
+  deriving stock (Generic, Prelude.Show, Prelude.Eq)
+{- ^ This hash is calculated from the `ApprovedBidders` value that the seller
+ fixes for the auction.
+-}
 
 -- Datums
 
-data AuctionEscrowDatum = AuctionEscrowDatum
+newtype AuctionEscrowDatum = AuctionEscrowDatum
   { auctionState :: AuctionState
   }
+  deriving stock (Generic, Prelude.Show, Prelude.Eq)
 
-data StandingBidDatum = StandingBidDatum
+newtype StandingBidDatum = StandingBidDatum
   { standingBid :: StandingBidState
   }
+  deriving stock (Generic, Prelude.Show, Prelude.Eq)
 
-data BidDepositDatum = BidDepositDatum
-  { bidder :: PubKeyHash
-  -- ^ Which bidder made this deposit?
+newtype BidDepositDatum = BidDepositDatum
+  { -- | Which bidder made this deposit?
+    bidder :: PubKeyHash
   }
+  deriving stock (Generic, Prelude.Show, Prelude.Eq)
 
 type AuctionFeeEscrowDatum = ()
 -- ^ This datum is empty because the auction terms have all the required info.
