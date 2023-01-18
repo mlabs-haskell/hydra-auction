515fc80e5b0eaba729aa92e775a497988624b96d
Giovanni Garufi <nazrhom@gmail.com>
2023-01-18 11:13:19 +0100

    Remove plutus-apps and refactor StandingBidValidator

--- src/HydraAuction/StandingBidValidator.hs
+++ src/HydraAuction/StandingBidValidator.hs
@@ -1,31 +1,35 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module HydraAuction.StandingBidValidator (
   mkStandingBidValidator,
+  validatorScript,
+  validatorHash,
 ) where
 
-import Plutus.V1.Ledger.Api (ScriptContext)
-import PlutusTx.Bool (Bool (False))
+import Plutus.Extras (ValidatorType, scriptValidatorHash, wrapValidator)
+import Plutus.V1.Ledger.Api (
+  Script,
+  ScriptContext,
+  ValidatorHash,
+  getValidator,
+  mkValidatorScript,
+ )
+import PlutusTx qualified
+import PlutusTx.Prelude
 
-import Ledger.Typed.Scripts qualified as Scripts
+import HydraAuction.Types (StandingBidDatum)
 
-import HydraAuction.Types (StandingBidDatum (StandingBidDatum))
-
--- | The type used for the TypedValidator of the idoValidator script
-data StandingBidValidatorTyped
-
-instance Scripts.ValidatorTypes StandingBidValidatorTyped where
-  type DatumType StandingBidValidatorTyped = StandingBidDatum
-  type RedeemerType StandingBidValidatorTyped = ()
-
+{-# INLINE mkStandingBidValidator #-}
 mkStandingBidValidator :: StandingBidDatum -> () -> ScriptContext -> Bool
-mkStandingBidValidator datum redeemer ctx = False
+mkStandingBidValidator _ _ _ = False
 
-standingBidValidatorTyped :: Scripts.TypedValidator StandingBidValidatorTyped
-standingBidValidatorTyped =
-  Scripts.mkTypedValidator @StandingBidValidatorTyped
-    ($$ PlutusTx.compile [||mkStakeValidatorScript||])
-    $$ PlutusTx.compile [||wrap||]
+validatorScript :: Script
+validatorScript = getValidator $ mkValidatorScript standingBidValidatorCompiled
+
+validatorHash :: ValidatorHash
+validatorHash = scriptValidatorHash validatorScript
+
+standingBidValidatorCompiled :: PlutusTx.CompiledCode ValidatorType
+standingBidValidatorCompiled = $$(PlutusTx.compile [||wrap mkStandingBidValidator||])
   where
-    wrap =
-      Scripts.wrapValidator
-        @StandingBidDatum
-        @()
+    wrap = wrapValidator @StandingBidDatum @()
