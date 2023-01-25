4b3ce49875f4cab50629d37c6eaccc042197d719
Gregory Gerasev <uhbif19@gmail.com>
2023-01-25 18:21:07 +0400

    Move back to field selectors in Addresses module

--- src/HydraAuction/Addresses.hs
+++ src/HydraAuction/Addresses.hs
@@ -1,7 +1,4 @@
 module HydraAuction.Addresses (
-  unStandingBidAddress,
-  unEscrowAddress,
-  unFeeEscrowAddress,
   VoucherCS (..),
   EscrowAddress (..),
   StandingBidAddress (..),
@@ -12,25 +9,16 @@ import Plutus.V1.Ledger.Api (Address)
 import Plutus.V1.Ledger.Value (CurrencySymbol)
 import PlutusTx qualified
 
-newtype VoucherCS = VoucherCS CurrencySymbol
+newtype VoucherCS = VoucherCS {unVoucherCS :: CurrencySymbol}
 
-newtype EscrowAddress = EscrowAddress Address
+newtype EscrowAddress = EscrowAddress {unEscrowAddress :: Address}
 
 PlutusTx.makeLift ''EscrowAddress
 
-unEscrowAddress :: EscrowAddress -> Address
-unEscrowAddress (EscrowAddress address) = address
-
-newtype StandingBidAddress = StandingBidAddress Address
+newtype StandingBidAddress = StandingBidAddress {unStandingBidAddress :: Address}
 
 PlutusTx.makeLift ''StandingBidAddress
 
-unStandingBidAddress :: StandingBidAddress -> Address
-unStandingBidAddress (StandingBidAddress address) = address
-
-newtype FeeEscrowAddress = FeeEscrowAddress Address
+newtype FeeEscrowAddress = FeeEscrowAddress {unFeeEscrowAddress :: Address}
 
 PlutusTx.makeLift ''FeeEscrowAddress
-
-unFeeEscrowAddress :: FeeEscrowAddress -> Address
-unFeeEscrowAddress (FeeEscrowAddress address) = address
