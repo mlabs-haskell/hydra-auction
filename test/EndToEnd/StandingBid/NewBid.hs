14a4b31540dd122eb7f4a16a9892a2d668a62fc6
George Flerovsky <george@flerovsky.ca>
2023-01-31 05:03:40 -0500

    Make tests use persistent subdir (ignore by git)

--- test/EndToEnd/StandingBid/NewBid.hs
+++ test/EndToEnd/StandingBid/NewBid.hs
@@ -40,6 +40,8 @@ import Test.Hydra.Prelude
 import Test.Tasty
 import Test.Tasty.Hspec
 
+import System.Directory (createDirectoryIfMissing, getCurrentDirectory)
+
 testSuite :: IO TestTree
 testSuite = testSpec "StandingBid > NewBid" spec
 
@@ -50,16 +52,25 @@ data Context = MkContext
 
 type Scenario a = ReaderT Context IO a
 
+stateDirectoryRelativePath :: FilePath
+stateDirectoryRelativePath = "node-state"
+
+getStateDirectory :: IO FilePath
+getStateDirectory = do
+  currentDirectory <- getCurrentDirectory
+  let stateDirectory = currentDirectory </> "node-state"
+  createDirectoryIfMissing True stateDirectory
+  pure stateDirectory
+
 runScenario :: Scenario () -> IO ()
-runScenario scenario =
-  withFile (workDir </> "test.log") ReadWriteMode $ \h ->
+runScenario scenario = do
+  stateDirectory <- getStateDirectory
+  withFile (stateDirectory </> "test.log") ReadWriteMode $ \h ->
     withTracerOutputTo h "Tracer" $ \tracer ->
       withCardanoNodeDevnet
         (contramap FromCardanoNode tracer)
-        workDir
+        stateDirectory
         $ \node -> runReaderT scenario (MkContext tracer node)
-  where
-    workDir = "."
 
 initWallet :: Actor -> Lovelace -> Scenario ()
 initWallet actor amount = do
