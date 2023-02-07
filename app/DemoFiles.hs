9fb3b81b95422e24f1ce0797bfacada4c14a11f5
George Flerovsky <george@flerovsky.ca>
2023-02-07 07:44:54 -0500

    Add DemoFiles

---
+++ app/DemoFiles.hs
@@ -0,0 +1,108 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module DemoFiles where
+
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Aeson qualified as Aeson
+import Data.ByteString qualified as BS
+import Data.ByteString.Lazy qualified as LBS
+
+import Prelude
+
+import GHC.Generics (Generic)
+import HydraAuction.Types
+import Plutus.V1.Ledger.Crypto (PubKeyHash)
+import Plutus.V1.Ledger.Time (POSIXTime (..))
+import Plutus.V1.Ledger.Value (AssetClass)
+import Plutus.V2.Ledger.Contexts (TxOutRef)
+
+import System.Directory
+import System.FilePath ((<.>), (</>))
+
+data AuctionTermsConfig = AuctionTermsConfig
+  { configDiffBiddingStart :: !Integer
+  , configDiffBiddingEnd :: !Integer
+  , configDiffVoucherExpiry :: !Integer
+  , configDiffCleanup :: !Integer
+  , configAuctionFee :: !Integer
+  , configStartingBid :: !Integer
+  , configMinimumBidIncrement :: !Integer
+  }
+  deriving stock (Generic, Prelude.Show, Prelude.Eq)
+
+instance ToJSON AuctionTermsConfig
+
+instance FromJSON AuctionTermsConfig
+
+data AuctionTermsDynamic = AuctionTermsDynamic
+  { configAuctionLot :: AssetClass
+  , configSeller :: !PubKeyHash
+  , configDelegates :: ![PubKeyHash]
+  , configUtxoRef :: !TxOutRef
+  , configAnnouncementTime :: !POSIXTime
+  }
+  deriving stock (Generic, Prelude.Show, Prelude.Eq)
+
+configToAuctionTerms ::
+  AuctionTermsConfig ->
+  AuctionTermsDynamic ->
+  Maybe AuctionTerms
+configToAuctionTerms AuctionTermsConfig {..} AuctionTermsDynamic {..} = do
+  auctionFee <- intToNatural configAuctionFee
+  startingBid <- intToNatural configStartingBid
+  minimumBidIncrement <- intToNatural configMinimumBidIncrement
+
+  pure
+    AuctionTerms
+      { auctionLot = configAuctionLot
+      , seller = configSeller
+      , delegates = configDelegates
+      , biddingStart = toAbsTime configDiffBiddingStart * 1000
+      , biddingEnd = toAbsTime configDiffBiddingEnd * 1000
+      , voucherExpiry = toAbsTime configDiffVoucherExpiry * 1000
+      , cleanup = toAbsTime configDiffCleanup * 1000
+      , auctionFee = auctionFee
+      , startingBid = startingBid
+      , minimumBidIncrement = minimumBidIncrement
+      , utxoRef = configUtxoRef
+      }
+  where
+    (POSIXTime announcementTime) = configAnnouncementTime
+    toAbsTime n = POSIXTime $ announcementTime + n
+
+-- =============================================================================
+-- Auction config and state directories
+newtype AuctionName = AuctionName String
+  deriving stock (Prelude.Show, Prelude.Eq)
+
+getRelativeDirectory :: FilePath -> IO FilePath
+getRelativeDirectory filepath = do
+  currentDirectory <- getCurrentDirectory
+  let dir = currentDirectory </> filepath
+  createDirectoryIfMissing True dir
+  pure dir
+
+getAuctionConfigDirectory :: IO FilePath
+getAuctionConfigDirectory =
+  getRelativeDirectory $ "example" </> "auction-config"
+
+getAuctionStateDirectory :: IO FilePath
+getAuctionStateDirectory =
+  getRelativeDirectory "auction-state"
+
+-- =============================================================================
+-- Read/write auction config files
+getAuctionConfigFilePath :: AuctionName -> IO FilePath
+getAuctionConfigFilePath (AuctionName auctionName) = do
+  auctionConfigDir <- getAuctionConfigDirectory
+  pure $ auctionConfigDir </> auctionName <.> "json"
+
+readAuctionTermsConfig :: AuctionName -> IO (Maybe AuctionTermsConfig)
+readAuctionTermsConfig auctionName = do
+  filepath <- getAuctionConfigFilePath auctionName
+  Aeson.decode . LBS.fromStrict <$> BS.readFile filepath
+
+writeAuctionTermsConfig :: AuctionName -> AuctionTermsConfig -> IO ()
+writeAuctionTermsConfig auctionName config = do
+  filepath <- getAuctionConfigFilePath auctionName
+  BS.writeFile filepath . LBS.toStrict . Aeson.encode $ config
