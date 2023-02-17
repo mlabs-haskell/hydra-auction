b16b83ea8004142e3e8cd6a382d47c597373cc3c
Gregory Gerasev <uhbif19@gmail.com>
2023-02-17 06:27:57 +0400

    Separate HydraAuction.Runner.Time module

--- test/EndToEnd/Utils.hs
+++ test/EndToEnd/Utils.hs
@@ -1,52 +1,14 @@
-module EndToEnd.Utils (mkAssertion, waitUntil) where
+module EndToEnd.Utils (mkAssertion) where
 
 -- Prelude imports
 import Hydra.Prelude hiding (threadDelay)
-
--- Haskell imports
-
-import Control.Concurrent (threadDelay)
 
 -- Haskell test imports
 import Test.Hydra.Prelude (failAfter)
 import Test.Tasty.HUnit (Assertion)
 
--- Plutus imports
-import Plutus.V1.Ledger.Time (POSIXTime (..))
-
--- Hydra imports
-import CardanoClient (queryTip)
-import CardanoNode (RunningNode (..))
-import Hydra.Cardano.Api (ChainPoint (..), SlotNo (..))
-
 -- Hydra auction imports
-import HydraAuction.Runner (
-  ExecutionContext (..),
-  Runner,
-  executeTestRunner,
- )
-import HydraAuction.Tx.Common (toSlotNo)
+import HydraAuction.Runner (Runner, executeTestRunner)
 
 mkAssertion :: Runner () -> Assertion
 mkAssertion = failAfter 60 . executeTestRunner
-
-waitUntil :: POSIXTime -> Runner ()
-waitUntil time = do
-  slotToWait <- toSlotNo time
-  waitUntilSlot slotToWait
-
-waitUntilSlot :: SlotNo -> Runner ()
-waitUntilSlot awaitedSlot = do
-  currentSlot' <- currentSlot
-  when (currentSlot' < awaitedSlot) $ do
-    liftIO $ threadDelay 1_000
-    waitUntilSlot awaitedSlot
-
-currentSlot :: Runner SlotNo
-currentSlot = do
-  MkExecutionContext {node} <- ask
-  tip <- liftIO $ queryTip (networkId node) (nodeSocket node)
-  return $
-    case tip of
-      ChainPointAtGenesis -> SlotNo 0
-      ChainPoint slotNo _ -> slotNo
