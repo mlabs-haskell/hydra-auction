6f321788ac7f25fbc083199be7d5852c604846a3
Gregory Gerasev <uhbif19@gmail.com>
2023-03-14 00:09:45 +0400

    Implement basic stub for Delegate server

--- delegate-app/Main.hs
+++ delegate-app/Main.hs
@@ -3,6 +3,42 @@ module Main (main) where
 -- Prelude imports
 import Prelude
 
+-- Haskell imports
+import Control.Concurrent.Async (withAsync)
+import Control.Concurrent.MVar (MVar, newEmptyMVar, putMVar, takeMVar)
+import Control.Monad (void)
+import Control.Monad.IO.Class (MonadIO (liftIO))
+
+-- HydraAuction imports
+import HydraAuction.Delegate (
+  DelegateEvent (Start),
+  DelegateInput (DelegateEvent),
+  DelegateRunnerT,
+  delegateStep,
+  execDelegateRunner,
+ )
+
+timedEventsProducer :: MVar DelegateInput -> IO ()
+timedEventsProducer eventChannel = do
+  putMVar eventChannel $ DelegateEvent Start
+
+consumer :: MVar DelegateInput -> IO ()
+consumer eventChannel =
+  -- Either ignored, because infinite loop cannot actually return
+  void $ execDelegateRunner go
+  where
+    go :: DelegateRunnerT IO ()
+    go = do
+      event <- liftIO $ takeMVar eventChannel
+      delegateResponse <- delegateStep event
+
+      -- FIXME: send response to client
+      liftIO $ putStrLn $ show delegateResponse
+      go
+
 main :: IO ()
 main = do
-  putStrLn "FIXME"
+  eventChannel <- newEmptyMVar
+
+  withAsync (timedEventsProducer eventChannel) $ \_ -> do
+    consumer eventChannel
