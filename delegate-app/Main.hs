a3e2f94fd7d9d4211c328cc7cba9989efeec2ad5
Giovanni Garufi <nazrhom@gmail.com>
2023-03-27 19:21:46 +0200

    Queue auction stage events in delegate server after a new auction is set

--- delegate-app/Main.hs
+++ delegate-app/Main.hs
@@ -5,45 +5,60 @@ import Prelude
 
 -- Haskell imports
 
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (Chan, newChan, readChan, threadDelay, writeChan)
+import Control.Concurrent.Async (async)
+import Control.Monad (forever, void)
 import Control.Monad.IO.Class (MonadIO (liftIO))
 
+-- Plutus imports
+import Plutus.V2.Ledger.Api (POSIXTime (..))
+
 -- HydraAuction imports
+
 import HydraAuction.Delegate (
-  DelegateEvent (Start),
+  DelegateEvent (..),
   DelegateInput (DelegateEvent),
   DelegateRunnerT,
   delegateStep,
   execDelegateRunnerT,
  )
+import HydraAuction.Delegate.Interface (DelegateResponse (..))
+import HydraAuction.OnChain.Common (secondsLeftInInterval, stageToInterval)
+import HydraAuction.Tx.Common (currentAuctionStage, currentTimeMilliseconds)
 
-eventsProducer :: Bool -> DelegateRunnerT IO (Maybe DelegateEvent)
-eventsProducer thisIsFirstRun =
-  if thisIsFirstRun
-    then return $ Just Start
-    else return Nothing
+consumer :: Chan DelegateInput -> DelegateRunnerT IO ()
+consumer events = do
+  -- This will block until an event appears in the queue
+  ev <- liftIO $ readChan events
+  delegateResponse <- delegateStep ev
+  liftIO $ mbQueueAuctionPhases delegateResponse events
+  -- FIXME: send response to client
+  liftIO $
+    putStrLn $
+      "Delegate responses for input: " <> show delegateResponse
 
-consumer :: Maybe DelegateInput -> DelegateRunnerT IO ()
-consumer mInput = case mInput of
-  Just input -> do
-    delegateResponse <- delegateStep input
-    -- FIXME: send response to client
-    liftIO $
-      putStrLn $
-        "Delegate responses for input: " <> show delegateResponse
-  Nothing -> return ()
+mbQueueAuctionPhases :: [DelegateResponse] -> Chan DelegateInput -> IO ()
+mbQueueAuctionPhases [AuctionSet terms] events = queueCurrentStage
+  where
+    queueCurrentStage = do
+      currentStage <- currentAuctionStage terms
+      writeChan events (DelegateEvent $ AuctionStageStarted currentStage)
+      currentPosixTime <- POSIXTime <$> currentTimeMilliseconds
+      let mSecsLeft = secondsLeftInInterval currentPosixTime (stageToInterval terms currentStage)
+      case mSecsLeft of
+        Nothing -> pure ()
+        Just s -> do
+          void $ async (threadDelay (fromInteger s * 1000) >> queueCurrentStage)
+mbQueueAuctionPhases _ _ = pure ()
 
-delegateTick :: Int
-delegateTick = 1_000
+spawnServer :: Chan DelegateInput -> IO ()
+spawnServer _ = forever $ pure ()
 
 main :: IO ()
 main = do
+  events <- newChan
+  -- Write init even in queue
+  writeChan events (DelegateEvent Start)
   -- FIXME: cover either case. It probably should not be transformer.
-  _ <- execDelegateRunnerT $ go True
+  _ <- execDelegateRunnerT $ forever (consumer events)
   return ()
-  where
-    go thisIsFirstRun = do
-      mEvent <- eventsProducer thisIsFirstRun
-      consumer (DelegateEvent <$> mEvent)
-      liftIO $ threadDelay delegateTick
-      go False
