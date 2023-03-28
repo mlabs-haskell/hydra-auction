84b309edf1d4df3e3c756ea1164f46d420aed159
Gregory Gerasev <uhbif19@gmail.com>
2023-03-28 23:17:56 +0300

    Separate HydraAuctionUtils.BundledData and add ProtocolParameters reading

---
+++ src-utils/HydraAuctionUtils/BundledData.hs
@@ -0,0 +1,35 @@
+module HydraAuctionUtils.BundledData (
+  readDataFile,
+  readHydraNodeProtocolParams,
+) where
+
+-- Prelude imports
+import Prelude
+
+-- Haskell imports
+import Data.Aeson qualified as Aeson
+import Data.ByteString qualified as BS
+import Paths_hydra_auction qualified as Pkg
+import System.FilePath ((</>))
+
+-- Hydra imports
+import Hydra.Cardano.Api (
+  ProtocolParameters,
+  ShelleyBasedEra (ShelleyBasedEraShelley),
+  fromLedgerPParams,
+ )
+
+-- | Lookup a config file similar reading a file from disk.
+readDataFile :: FilePath -> IO BS.ByteString
+readDataFile source = do
+  filename <- Pkg.getDataFileName ("data" </> source)
+  BS.readFile filename
+
+readHydraNodeProtocolParams :: IO ProtocolParameters
+readHydraNodeProtocolParams = do
+  bytes <- readDataFile "protocol-parameters.json"
+  case Aeson.eitherDecodeStrict bytes of
+    Left errorMsg ->
+      fail $
+        "Cannot decode protocol-parameters.json, error: " <> show errorMsg
+    Right protocolParams -> return protocolParams
