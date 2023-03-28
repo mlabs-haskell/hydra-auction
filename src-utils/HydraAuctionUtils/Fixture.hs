7414c6c9b661e5456284dceadb631851c9591cae
Gregory Gerasev <uhbif19@gmail.com>
2023-03-28 23:17:56 +0300

    Separate HydraAuctionUtils.BundledData and add ProtocolParameters reading

--- src-utils/HydraAuctionUtils/Fixture.hs
+++ src-utils/HydraAuctionUtils/Fixture.hs
@@ -27,10 +27,11 @@ import Plutus.V1.Ledger.Crypto (PubKeyHash)
 -- Haskell imports
 import Data.Aeson qualified as Aeson
 import Data.Bifunctor (first)
-import Data.ByteString qualified as BS
 import GHC.Generics (Generic)
-import Paths_hydra_auction qualified as Pkg
 import System.FilePath ((<.>), (</>))
+
+-- Hydra Auction imports
+import HydraAuctionUtils.BundledData (readDataFile)
 
 -- | Enumeration of known actors for which we can get the 'keysFor' and 'writeKeysFor'.
 data Actor
@@ -50,7 +51,7 @@ instance Aeson.ToJSON Actor
 -- | Get the "well-known" keys for given actor.
 keysFor :: Actor -> IO (VerificationKey PaymentKey, SigningKey PaymentKey)
 keysFor actor = do
-  bs <- readConfigFile ("credentials" </> actorName actor <.> "sk")
+  bs <- readDataFile ("credentials" </> actorName actor <.> "sk")
   let res =
         first TextEnvelopeAesonDecodeError (Aeson.eitherDecodeStrict bs)
           >>= deserialiseFromTextEnvelope asSigningKey
@@ -62,11 +63,6 @@ keysFor actor = do
     asSigningKey :: AsType (SigningKey PaymentKey)
     asSigningKey = AsSigningKey AsPaymentKey
 
--- | Lookup a config file similar reading a file from disk.
-readConfigFile :: FilePath -> IO BS.ByteString
-readConfigFile source = do
-  filename <- Pkg.getDataFileName ("data" </> source)
-  BS.readFile filename
 
 getActorVkHash :: Actor -> IO PubKeyHash
 getActorVkHash actor = do
