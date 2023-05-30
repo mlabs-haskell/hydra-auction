3481c23909bcdacdda9bf52993941547c7a98c2f
Gregory Gerasev <uhbif19@gmail.com>
2023-05-31 02:24:37 +0900

    Refactoring: use ServerOutput from Hydra API

--- src-utils/HydraAuctionUtils/Hydra/Runner.hs
+++ src-utils/HydraAuctionUtils/Hydra/Runner.hs
@@ -1,7 +1,6 @@
 module HydraAuctionUtils.Hydra.Runner (
   HydraRunner (..),
   HydraExecutionContext (..),
-  matchingHydraEvent,
   executeHydraRunnerFakingParams,
   executeHydraRunner,
 ) where
@@ -11,30 +10,18 @@ import Prelude
 
 -- Haskell imports
 
-import Control.Lens ((^?))
-import Control.Lens.Getter (Getting)
 import Control.Monad (guard)
 import Control.Monad.Catch (MonadCatch, MonadMask, MonadThrow, try)
 import Control.Monad.Reader (MonadReader, ReaderT (runReaderT), ask)
 import Control.Monad.Trans (MonadIO (liftIO))
 import Control.Tracer (Tracer, contramap, stdoutTracer, traceWith)
 import Data.Aeson (
-  FromJSON,
-  Key,
-  Value,
-  parseJSON,
+  Result (..),
+  fromJSON,
   toJSON,
-  (.=),
  )
-import Data.Aeson.Lens (key)
-import Data.Aeson.Types (parseMaybe)
-import Data.Map qualified as Map
-import Data.Monoid (First)
 import GHC.Natural (Natural, naturalToInt)
 import Test.HUnit.Lang (HUnitFailure)
-
--- Cardano imports
-import Cardano.Api.UTxO qualified as UTxO
 
 -- Hydra imports
 import Cardano.Api (NetworkId (Testnet))
@@ -43,9 +30,9 @@ import Hydra.Cardano.Api (
   pattern TxValidityNoLowerBound,
   pattern TxValidityNoUpperBound,
  )
+import Hydra.Chain.Direct.State ()
 import HydraNode (
   HydraClient,
-  input,
   send,
   waitMatch,
  )
@@ -54,8 +41,8 @@ import HydraNode (
 
 import HydraAuctionUtils.BundledData (readHydraNodeProtocolParams)
 import HydraAuctionUtils.Hydra.Interface (
-  HydraCommand (..),
-  HydraEvent (..),
+  HydraCommand,
+  HydraEvent,
   getHydraEventKind,
  )
 import HydraAuctionUtils.Hydra.Monad (
@@ -119,12 +106,15 @@ instance MonadHydra HydraRunner where
     where
       timeout = fromInteger $ toInteger $ naturalToInt timeoutSeconds
       matchingHandler value = do
-        event <- matchingHydraEvent value
+        event <- case fromJSON value of
+          Success x -> Just x
+          Error _ -> Nothing
         guard $ matchingPredicate event
         return event
       matchingPredicate event = case awaitedSpec of
         Any -> True
-        SpecificKind expectedKind -> getHydraEventKind event == expectedKind
+        SpecificKind expectedKind ->
+          getHydraEventKind event == expectedKind
         SpecificEvent expectedEvent -> event == expectedEvent
         CustomMatcher (EventMatcher customMatcher) -> customMatcher event
 
@@ -147,55 +137,6 @@ instance MonadBlockchainParams HydraRunner where
   convertValidityBound (_, _) =
     return (TxValidityNoLowerBound, TxValidityNoUpperBound)
 
-matchingHydraEvent :: Value -> Maybe HydraEvent
-matchingHydraEvent value =
-  case value ^? key "tag" of
-    Just "GetUTxOResponse" -> getUtxoValueHandler
-    Just "HeadIsInitializing" ->
-      HeadIsInitializing <$> retrieveField "headId"
-    Just "TxSeen" -> TxSeen <$> retrieveField "transaction"
-    Just "TxValid" -> TxValid <$> retrieveField "transaction"
-    Just "TxInvalid" ->
-      TxInvalid <$> retrieveField "transaction" <*> retrieveField "utxo"
-    Just "InvalidInput" ->
-      InvalidInput <$> retrieveField "reason" <*> retrieveField "input"
-    Just "PostTxOnChainFailed" ->
-      PostTxOnChainFailed
-        <$> retrieveField' (key "postChainTx" . key "tag")
-        <*> retrieveField' (key "postTxError" . key "tag")
-    Just "CommandFailed" ->
-      CommandFailed
-        <$> retrieveField'
-          (key "clientInput" . key "tag")
-    Just "SnapshotConfirmed" ->
-      SnapshotConfirmed
-        <$> retrieveField'
-          ( key "snapshot" . key "confirmedTransactions"
-          )
-        <*> retrieveField'
-          ( key "snapshot" . key "utxo"
-          )
-    Just "Committed" ->
-      Committed <$> retrieveField "utxo" <*> retrieveField "party"
-    Just "HeadIsOpen" -> HeadIsOpen <$> retrieveField "utxo"
-    Just "HeadIsClosed" -> Just HeadIsClosed
-    Just "ReadyToFanout" -> Just ReadyToFanout
-    Just "HeadIsFinalized" -> HeadIsFinalized <$> retrieveField "utxo"
-    Just "HeadIsAborted" -> Just HeadIsAborted
-    _ -> Nothing
-  where
-    getUtxoValueHandler =
-      GetUTxOResponse <$> do
-        txInOutMap <- retrieveField "utxo"
-        return $ UTxO.fromPairs $ Map.toList txInOutMap
-    retrieveField :: FromJSON a => Key -> Maybe a
-    retrieveField fieldKey = retrieveField' $ key fieldKey
-    retrieveField' ::
-      FromJSON a => Getting (First Value) Value Value -> Maybe a
-    retrieveField' getter = do
-      fieldValue <- value ^? getter
-      parseMaybe parseJSON fieldValue
-
 executeHydraRunner ::
   HydraExecutionContext ->
   HydraRunner a ->
@@ -213,7 +154,7 @@ executeHydraRunnerFakingParams node monad = do
           }
   liftIO $ executeHydraRunner (context patchedParams) monad
   where
-    tracer = contramap show stdoutTracer
+    tracer = contramap (const "TODO") stdoutTracer
     context params =
       MkHydraExecutionContext
         { node = node
