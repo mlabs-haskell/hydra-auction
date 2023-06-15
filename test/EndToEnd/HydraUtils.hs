c63e6868e6e05fec1963f47641621adc9a59db7d
Gregory Gerasev <uhbif19@gmail.com>
2023-06-16 04:15:43 +0900

    Refactoring: delegate emulator change

--- test/EndToEnd/HydraUtils.hs
+++ test/EndToEnd/HydraUtils.hs
@@ -1,7 +1,5 @@
 -- FIXME: rename to Emulator
 module EndToEnd.HydraUtils (
-  spinUpHeads,
-  runningThreeNodesDockerComposeHydra,
   runCompositeForDelegate,
   EmulatorDelegate (..),
   EmulatorContext (..),
@@ -37,7 +35,7 @@ import Hydra.Cluster.Faucet (Marked (..), seedFromFaucet_)
 import Hydra.ContestationPeriod (
   ContestationPeriod (UnsafeContestationPeriod),
  )
-import HydraNode (EndToEndLog (..), HydraClient (..), waitForNodesConnected, withHydraCluster)
+import HydraNode (HydraClient (..), waitForNodesConnected, withHydraCluster)
 import Test.Hydra.Prelude (withTempDir)
 
 -- HydraAuction imports
@@ -71,88 +69,47 @@ import HydraAuctionUtils.L1.Runner (
 -- This function will set the HYDRA_CONFIG_DIR env var locally
 -- This is required so the hydra nodes pick up on the correct protocol-parameters.json
 -- file.
-spinUpHeads :: Int -> TxId -> (EmulatorDelegateClients -> L1Runner ()) -> L1Runner ()
-spinUpHeads clusterIx hydraScriptsTxId cont = do
+withManualHydraCluster :: Int -> TxId -> ([HydraClient] -> L1Runner ()) -> L1Runner ()
+withManualHydraCluster clusterIx hydraScriptsTxId cont = do
   liftIO $ do
     hydraDir <- lookupProtocolParamPath
     setEnv "HYDRA_CONFIG_DIR" hydraDir
   ctx@(MkExecutionContext {node}) <- ask
-  let hydraTracer = nullTracer
   liftIO $
     withTempDir "end-to-end-test" $ \tmpDir -> do
-      oscarKeys@(oscarCardanoVk, _) <- keysFor Oscar
-      patriciaKeys@(patriciaCardanoVk, _) <- keysFor Patricia
-      rupertKeys@(rupertCardanoVk, _) <- keysFor Rupert
-
-      (_, oscarSk) <- hydraKeysFor Oscar
-      (_, patriciaSk) <- hydraKeysFor Patricia
-      (_, rupertSk) <- hydraKeysFor Rupert
-
-      let cardanoKeys = [oscarKeys, patriciaKeys, rupertKeys]
-          hydraKeys = [oscarSk, patriciaSk, rupertSk]
-
+      let actors = (Map.!) actorsByKind HydraNodeActor
+      cardanoKeys <- mapM keysFor actors
+      hydraSks <- mapM (fmap snd . hydraKeysFor) actors
       let firstNodeId = clusterIx * 3
       let contestationPeriod = UnsafeContestationPeriod 2
       withHydraCluster
-        hydraTracer
+        nullTracer
         tmpDir
         (nodeSocket node)
         firstNodeId
         cardanoKeys
-        hydraKeys
+        hydraSks
         hydraScriptsTxId
         contestationPeriod
         $ \nodes -> do
-          [n1, n2, n3] <- pure $ toList nodes
-          waitForNodesConnected hydraTracer [n1, n2, n3]
-          -- Funds to be used as fuel by Hydra protocol transactions + some other ada to use as collateral to commit into the head
-          let faucetTracer = contramap FromFaucet hydraTracer
-          seedFromFaucet_ node oscarCardanoVk 100_000_000 Fuel faucetTracer
-          seedFromFaucet_ node patriciaCardanoVk 100_000_000 Fuel faucetTracer
-          seedFromFaucet_ node rupertCardanoVk 100_000_000 Fuel faucetTracer
-          seedFromFaucet_ node oscarCardanoVk 100_000_000 Normal faucetTracer
-          seedFromFaucet_ node patriciaCardanoVk 100_000_000 Normal faucetTracer
-          seedFromFaucet_ node rupertCardanoVk 100_000_000 Normal faucetTracer
-          [actor1, actor2, actor3] <-
-            return $
-              (Map.!) actorsByKind HydraNodeActor
-          executeL1Runner ctx $ do
-            let threeClients =
-                  Map.fromList
-                    [ (Main, (n1, actor1))
-                    , (Second, (n2, actor2))
-                    , (Third, (n3, actor3))
-                    ]
-             in cont threeClients
-
-runningThreeNodesDockerComposeHydra ::
-  (EmulatorDelegateClients -> L1Runner b) ->
-  IO b
-runningThreeNodesDockerComposeHydra cont = do
-  _ <- system "./scripts/spin-up-new-devnet.sh 0"
-
-  -- FIXME: more relaible wait (not sure for what, guess sockets opening)
-  threadDelay 2_000_000
-
-  [actor1, actor2, actor3] <-
-    return $
-      (Map.!) actorsByKind HydraNodeActor
-
-  runHydraClientN 1 $
-    \n1 -> runHydraClientN 2 $
-      \n2 -> runHydraClientN 3 $
-        \n3 ->
-          let threeClients =
-                Map.fromList
-                  [ (Main, (n1, actor1))
-                  , (Second, (n2, actor2))
-                  , (Third, (n3, actor3))
-                  ]
-           in finally
-                (executeL1RunnerWithNode dockerNode (cont threeClients))
-                (system "./scripts/stop-demo.sh")
+          waitForNodesConnected nullTracer $ toList nodes
+          seedHydraNodes node cardanoKeys
+          executeL1Runner ctx $ cont $ toList nodes
   where
-    runHydraClientN n cont' = liftIO $
+    seedHydraNodes node cardanoKeys = do
+      let initActor mark key =
+            seedFromFaucet_ node key 100_000_000 mark nullTracer
+      mapM_ (initActor Fuel . fst) cardanoKeys
+      mapM_ (initActor Normal . fst) cardanoKeys
+
+withDockerComposeCluster ::
+  ([HydraClient] -> L1Runner b) ->
+  IO b
+withDockerComposeCluster cont = do
+  _ <- system "./scripts/spin-up-new-devnet.sh 0"
+  withNClients 3 action
+  where
+    withHydraClientN n cont' = liftIO $
       runClient "127.0.0.1" (4000 + n) "/history=no" $
         \connection ->
           cont' $
@@ -161,6 +118,16 @@ runningThreeNodesDockerComposeHydra cont = do
               , connection = connection
               , tracer = contramap show nullTracer
               }
+    withNClients' clientsAcc n cont' =
+      if length clientsAcc == n
+        then cont' clientsAcc
+        else withHydraClientN n $
+          \client -> withNClients' (client : clientsAcc) n cont'
+    withNClients = withNClients' []
+    action' nodes = do
+      waitForNodesConnected nullTracer nodes
+      executeL1RunnerWithNode dockerNode (cont nodes)
+    action nodes = finally (action' nodes) (system "./scripts/stop-demo.sh")
 
 data EmulatorDelegate = Main | Second | Third
   deriving stock (Eq, Show, Enum, Bounded, Ord)
@@ -195,14 +162,18 @@ runEmulatorInTest action = do
   if useDockerForTests
     then
       liftIO $
-        runningThreeNodesDockerComposeHydra $
-          flip runEmulator action
+        withDockerComposeCluster $
+          flip runEmulator action . makeThreeClients actors
     else executeTestL1Runner $ do
       MkExecutionContext {node} <- ask
       -- FIXME: race condition on Faucet
       (!hydraScriptsTxId, !_) <- liftIO $ prepareScriptRegistry node
-      spinUpHeads 0 hydraScriptsTxId $
-        flip runEmulator action
+      withManualHydraCluster 0 hydraScriptsTxId $
+        flip runEmulator action . makeThreeClients actors
+  where
+    actors = (Map.!) actorsByKind HydraNodeActor
+    makeThreeClients actors nodes =
+      Map.fromList $ zip allDelegates (zip nodes actors)
 
 runEmulator :: EmulatorDelegateClients -> DelegatesClusterEmulator a -> L1Runner a
 runEmulator clients action = do
