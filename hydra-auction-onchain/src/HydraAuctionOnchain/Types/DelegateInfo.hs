bab36de5fd7761127eb7f0bf109e9feace2871db
George Flerovsky <george@flerovsky.ca>
2023-11-25 19:30:25 -0500

    Add DelegateInfo on-chain type

---
+++ hydra-auction-onchain/src/HydraAuctionOnchain/Types/DelegateInfo.hs
@@ -0,0 +1,49 @@
+module HydraAuctionOnchain.Types.DelegateInfo (
+  DelegateInfo (..),
+  DelegateInfo'Error (..),
+  validDelegateInfo,
+) where
+
+import PlutusTx.Prelude
+
+import PlutusLedgerApi.V1 (PubKeyHash)
+import PlutusTx qualified
+
+import HydraAuctionOnchain.Lib.Error (eCode, err)
+
+import HydraAuctionOnchain.Types.DelegateInfoError (DelegateInfo'Error (..))
+
+data DelegateInfo = DelegateInfo
+  { di'GroupName :: BuiltinByteString
+  , di'GroupURL :: BuiltinByteString
+  , di'Delegates :: [PubKeyHash]
+  -- ^ The payment vkey hashes of the individual delegates in this group.
+  -- These payment vkey hashes also indicate where the delegates
+  -- should receive payment for their services.
+  }
+
+-- -------------------------------------------------------------------------
+-- Validation
+-- -------------------------------------------------------------------------
+
+{-# INLINEABLE validDelegateInfo #-}
+validDelegateInfo ::
+  DelegateInfo ->
+  Bool
+validDelegateInfo DelegateInfo {..} =
+  --
+  -- (DI01) There must be at least one delegate.
+  (length di'Delegates > 0)
+    `err` $(eCode DelegateInfo'Error'NoDelegates)
+
+-- -------------------------------------------------------------------------
+-- Plutus instances
+-- -------------------------------------------------------------------------
+PlutusTx.unstableMakeIsData ''DelegateInfo
+
+instance Eq DelegateInfo where
+  (DelegateInfo x1 x2 x3)
+    == (DelegateInfo y1 y2 y3) =
+      x1 == y1
+        && x2 == y2
+        && x3 == y3
